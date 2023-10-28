ba19e0f718461ca94ad26fee197244f48f8af224
Jhonn-Lopez <jho.lopezm@duocuc.cl>
2023-10-27 22:26:27 -0300

    Carga de models

--- FirstDjango/userAPI/models.py
+++ FirstDjango/userAPI/models.py
@@ -1,1 +1,40 @@
-# Create your models here.
+from django.db import models
+
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    email = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+class ActiveCurse(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    curse = models.ForeignKey('Curse', on_delete=models.CASCADE)
+    is_active = models.CharField(max_length=1)
+
+class Alternative(models.Model):
+    alt_id = models.IntegerField(primary_key=True)
+    alt_desc = models.CharField(max_length=250)  # Asegúrate de ajustar la longitud según tus necesidades
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+
+class CorrectAnswer(models.Model):
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    alternative = models.ForeignKey(Alternative, on_delete=models.CASCADE)
+
+class Curse(models.Model):
+    curse_id = models.IntegerField(primary_key=True)
+    cur_name = models.CharField(max_length=250)
+    cur_desc = models.CharField(max_length=1000)
+    module = models.ForeignKey('Module', on_delete=models.CASCADE)
+
+class Module(models.Model):
+    module_id = models.IntegerField(primary_key=True)
+    mod_name = models.CharField(max_length=250)
+    mod_desc = models.CharField(max_length=1000)
+    video = models.BinaryField(null=True)  # Puedes utilizar BinaryField para el campo "video"
+    text = models.TextField()  # Utilizamos TextField para el campo "text"
+
+class Question(models.Model):
+    question_id = models.IntegerField(primary_key=True)
+    question = models.CharField(max_length=500)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
