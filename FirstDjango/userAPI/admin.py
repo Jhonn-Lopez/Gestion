8ce845504e6b955293bf535ff8d60d0c2655b227
Jose Palomo <josepalriv@gmail.com>
2023-11-04 22:26:59 -0300

    se agregan campos en admin

--- FirstDjango/userAPI/admin.py
+++ FirstDjango/userAPI/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+from .models import User, ActiveCurse, Alternative, CorrectAnswer, Curse, Module, Question
+
+admin.site.register(User)
+admin.site.register(ActiveCurse)
+admin.site.register(Alternative)
+admin.site.register(CorrectAnswer)
+admin.site.register(Curse)
+admin.site.register(Module)
+admin.site.register(Question)
